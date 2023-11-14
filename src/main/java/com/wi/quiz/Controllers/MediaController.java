def0b584dbd94ed3732a1db195463b7cc28ca2d6
MAROUANE <marwaneaitelhaj001@gmail.com>
2023-11-14 22:36:01 +0100

    Add CRUD operations for Media and QuestionOfQuiz DTOs

--- src/main/java/com/wi/quiz/Controllers/MediaController.java
+++ src/main/java/com/wi/quiz/Controllers/MediaController.java
@@ -1,2 +1,60 @@
-package com.wi.quiz.Controllers;public class MediaController {
+package com.wi.quiz.Controllers;
+
+import com.wi.quiz.DTO.MediaDto;
+import com.wi.quiz.DTO.MediaDto;
+import com.wi.quiz.DTO.Rsp.MediaDtoRsp;
+import com.wi.quiz.Services.Impl.MediaServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/media")
+public class MediaController {
+    @Autowired
+    private MediaServiceImpl mediaService;
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody MediaDto media) {
+        Map<String, Object> message = new HashMap<>();
+        MediaDto mediaDto =  mediaService.save(media);
+        message.put("message", "Level created successfully");
+        message.put("media", mediaDto);
+        return new ResponseEntity<>(message, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MediaDtoRsp>> findAll() {
+        return ResponseEntity.ok(mediaService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MediaDtoRsp> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(mediaService.findOne(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@RequestBody MediaDto mediaDto, @PathVariable Long id) {
+        Map<String, Object> message = new HashMap<>();
+        MediaDto media = mediaService.update(mediaDto, id);
+        message.put("message", "Level updated successfully");
+        message.put("media", media);
+        return new ResponseEntity<>(message, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        Map<String, Object> message = new HashMap<>();
+        if (mediaService.delete(id)) {
+            message.put("message", "Level deleted successfully");
+            return new ResponseEntity<>(message, HttpStatus.OK);
+        } else {
+            message.put("message", "Level not deleted");
+            return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+        }
+    }
 }
--- src/main/java/com/wi/quiz/Controllers/QuestionOfQuizController.java
+++ src/main/java/com/wi/quiz/Controllers/QuestionOfQuizController.java
@@ -1,2 +1,60 @@
-package com.wi.quiz.Controllers;public class QuestionOfQuizController {
+package com.wi.quiz.Controllers;
+
+import com.wi.quiz.DTO.QuestionOfQuizDto;
+import com.wi.quiz.DTO.QuestionOfQuizDto;
+import com.wi.quiz.DTO.Rsp.QuestionOfQuizDtoRsp;
+import com.wi.quiz.Services.Impl.QuestionOfQuizServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/questionofquizDto")
+public class QuestionOfQuizController {
+    @Autowired
+    private QuestionOfQuizServiceImpl questionOfQuizService;
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody QuestionOfQuizDto questionOfQuiz) {
+        Map<String, Object> message = new HashMap<>();
+        QuestionOfQuizDto questionOfQuizDto =  questionOfQuizService.save(questionOfQuiz);
+        message.put("message", "Level created successfully");
+        message.put("questionOfQuiz", questionOfQuizDto);
+        return new ResponseEntity<>(message, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<QuestionOfQuizDtoRsp>> findAll() {
+        return ResponseEntity.ok(questionOfQuizService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<QuestionOfQuizDtoRsp> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(questionOfQuizService.findOne(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@RequestBody QuestionOfQuizDto questionOfQuizDto, @PathVariable Long id) {
+        Map<String, Object> message = new HashMap<>();
+        QuestionOfQuizDto questionOfQuiz = questionOfQuizService.update(questionOfQuizDto, id);
+        message.put("message", "Level updated successfully");
+        message.put("questionOfQuiz", questionOfQuiz);
+        return new ResponseEntity<>(message, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        Map<String, Object> message = new HashMap<>();
+        if (questionOfQuizService.delete(id)) {
+            message.put("message", "Level deleted successfully");
+            return new ResponseEntity<>(message, HttpStatus.OK);
+        } else {
+            message.put("message", "Level not deleted");
+            return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+        }
+    }
 }
