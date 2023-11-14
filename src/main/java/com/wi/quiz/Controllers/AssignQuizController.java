c370ac4a0f063017147fbfa8f68399d754948193
MAROUANE <marwaneaitelhaj001@gmail.com>
2023-11-14 22:24:51 +0100

    Add CRUD operations to AssignQuizController

--- src/main/java/com/wi/quiz/Controllers/AssignQuizController.java
+++ src/main/java/com/wi/quiz/Controllers/AssignQuizController.java
@@ -1,2 +1,59 @@
-package com.wi.quiz.Controllers;public class AssignQuizDtoController {
+package com.wi.quiz.Controllers;
+
+import com.wi.quiz.DTO.AssignQuizDto;
+import com.wi.quiz.DTO.Rsp.AssignQuizDtoRsp;
+import com.wi.quiz.Services.Impl.AssignQuizServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/assignquiz")
+public class AssignQuizController {
+    @Autowired
+    private AssignQuizServiceImpl quizService;
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody AssignQuizDto assignQuiz) {
+        Map<String, Object> message = new HashMap<>();
+        AssignQuizDto assignQuizDto =  quizService.save(assignQuiz);
+        message.put("message", "Level created successfully");
+        message.put("assignQuiz", assignQuizDto);
+        return new ResponseEntity<>(message, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AssignQuizDtoRsp>> findAll() {
+        return ResponseEntity.ok(quizService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AssignQuizDtoRsp> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(quizService.findOne(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@RequestBody AssignQuizDto assignQuizDto, @PathVariable Long id) {
+        Map<String, Object> message = new HashMap<>();
+        AssignQuizDto assignQuiz = quizService.update(assignQuizDto, id);
+        message.put("message", "Level updated successfully");
+        message.put("assignQuiz", assignQuiz);
+        return new ResponseEntity<>(message, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        Map<String, Object> message = new HashMap<>();
+        if (quizService.delete(id)) {
+            message.put("message", "Level deleted successfully");
+            return new ResponseEntity<>(message, HttpStatus.OK);
+        } else {
+            message.put("message", "Level not deleted");
+            return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+        }
+    }
 }
