46d227d56bc2a0a51bfb7795a4b590aea995b44a
MAROUANE <marwaneaitelhaj001@gmail.com>
2024-01-02 09:14:07 +0100

    Add PersonDtoLogin and PersonServiceImpl classes

---
+++ src/main/java/com/wi/quiz/DTO/Person/PersonDtoLogin.java
@@ -0,0 +1,13 @@
+package com.wi.quiz.DTO.Person;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalDate;
+@Data
+public class PersonDtoLogin {
+    @NotNull
+    protected String lastName;
+    @NotNull
+    protected String firstName;
+}
